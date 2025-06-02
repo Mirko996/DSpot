8dc2023483f7126cee73943e0c754e2576ef43c2
Mirko <mirkot316@gmail.com>
2025-06-02 11:23:30 +0200

    Initial commit DSpot

---
+++ src/main/java/com/SimpleSoft/dspot/DSpot/DSpotApplication.java
@@ -0,0 +1,13 @@
+package com.SimpleSoft.dspot.DSpot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DSpotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DSpotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/SimpleSoft/dspot/DSpot/DSpotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SimpleSoft.dspot.DSpot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DSpotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
